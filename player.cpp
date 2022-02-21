5323ccb52b6b36735d281fd94d29c64e1922a225
kunnnku <99231736+kunnnku@users.noreply.github.com>
2022-02-21 14:43:01 +0900

    bg.cppの当たり判定の変更/bg.hにMAX_MAP_STAGEのマクロ定義追加/Player.cppにジャンプ処理(仮)・マップ移動処理の追加

--- player.cpp
+++ player.cpp
@@ -14,6 +14,8 @@
 #include "score.h"
 #include "sound.h"
 #include "bg.h"
+#include "collision.h"
+#include "Physics.h"
 
 
 //*****************************************************************************
@@ -36,6 +38,15 @@ static float g_CharaUV = 0.0f;
 static int g_AnimePtn = 0;
 static int g_AnimeWaitFrame = 0;
 
+//重力部分
+static bool g_GravityUse = false;
+static float g_Gravity = 0.0f;
+//ジャンプ部分
+static bool g_JumpUse = false;
+
+//ステージ移動
+static int g_MoveStage = 0;
+
 //=============================================================================
 // 初期化処理
 //=============================================================================
@@ -44,16 +55,20 @@ HRESULT InitPlayer(void)
 	//テクスチャ読み込み
 	g_Player.texNo = LoadTexture("data/TEXTURE/majo.png");
 
-	//
+	//SE読み込み
 	g_ShotSENo = LoadSound("data/SE/shot000.wav");
 
 	//初期化
 	g_Player.pos.x = SCREEN_WIDTH / 2;
-	g_Player.pos.y = 440;
+	g_Player.pos.y = 370;
+	g_Player.oldpos = D3DXVECTOR2(0.0f, 0.0f);
 	g_Player.w = 50.0f;
 	g_Player.h = 50.0f;
 	g_Player.use = true;
 
+	//重力初期化
+	g_GravityUse = true;
+	g_Gravity = Gravity(1.0f);
 	return S_OK;
 }
 
@@ -70,47 +85,89 @@ void UninitPlayer(void)
 //=============================================================================
 void UpdatePlayer(void)
 {
-	if (GetKeyboardPress(DIK_UP))
+	if (g_Player.pos != g_Player.oldpos)
 	{
-		if (GetMapEnter(D3DXVECTOR2(g_Player.pos.x, g_Player.pos.y - 1.0f)) != 1)
-			g_Player.pos.y -= 1.0f;
+		g_Player.oldpos = g_Player.pos;
+	}
+	if (GetKeyboardPress(DIK_W))
+	{
+		g_JumpUse = true;
+
 
 		g_CharaUV = 0.75f;
 	}
 
-	if (GetKeyboardPress(DIK_DOWN))
+	/*if (GetKeyboardPress(DIK_S))
 	{
 		if (GetMapEnter(D3DXVECTOR2(g_Player.pos.x, g_Player.pos.y + 1.0f)) != 1)
 			g_Player.pos.y += 1.0f;
 
 		g_CharaUV = 0.0f;
-	}
+	}*/
 
-	if (GetKeyboardPress(DIK_LEFT))
+	if (GetKeyboardPress(DIK_A))
 	{
 		if (GetMapEnter(D3DXVECTOR2(g_Player.pos.x - 1.0f, g_Player.pos.y)) != 1)
-			g_Player.pos.x -= 1.0f;
+			g_Player.pos.x -= 3.0f;
 
 		g_CharaUV = 0.25f;
 	}
 
-	if (GetKeyboardPress(DIK_RIGHT))
+	if (GetKeyboardPress(DIK_D))
 	{
 		if (GetMapEnter(D3DXVECTOR2(g_Player.pos.x + 1.0f, g_Player.pos.y)) != 1)
-			g_Player.pos.x += 1.0f;
+			g_Player.pos.x += 3.0f;
 
 		g_CharaUV = 0.5f;
 	}
 
-	//マップ1へ切り替える
-	if (GetKeyboardTrigger(DIK_L))
+	if (g_JumpUse == true)
 	{
-		SetCurrentMap(1);
+		g_Player.pos.y -= 3.0f;
+		if (g_Player.pos.y <= 320.0f)
+		{
+			g_JumpUse = false;
+		}
+
 	}
-	//マップ0へ切り替える
-	if (GetKeyboardTrigger(DIK_K))
+
+	if (g_GravityUse == true)
 	{
-		SetCurrentMap(0);
+		g_Player.pos.y += g_Gravity;
+	}
+	if (CollisionBB(g_Player.pos, D3DXVECTOR2((SCREEN_WIDTH / 2), 480.0f), D3DXVECTOR2((g_Player.w), (g_Player.h)), D3DXVECTOR2((SCREEN_WIDTH + 160.0f), 160.0f)) == true)
+	{
+		g_GravityUse = false;
+		g_Gravity = 0.0f;
+	}
+	else
+	{
+		g_Gravity = Gravity(1.0f);
+		g_GravityUse = true;
+	}
+
+
+	//次マップへ切り替える
+	if (g_Player.pos.x > SCREEN_WIDTH)
+	{
+		g_Player.pos.x = 8.0f;
+		g_MoveStage += 1;
+		if (g_MoveStage > (MAX_MAP_STAGE - 1))
+		{
+			g_MoveStage = g_MoveStage - 1;
+		}
+		SetCurrentMap(g_MoveStage);
+	}
+	//前マップへ切り替える
+	if (g_Player.pos.x < 0.0f)
+	{
+		g_Player.pos.x = (SCREEN_WIDTH - 8.0f);
+		g_MoveStage -= 1;
+		if (g_MoveStage < 0)
+		{
+			g_MoveStage = g_MoveStage + 1;
+		}
+		SetCurrentMap(g_MoveStage);
 	}
 
 
