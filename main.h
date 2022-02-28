0204c796a81afe085b603014d426c82bf916de02
kunnnku <99231736+kunnnku@users.noreply.github.com>
2022-02-28 16:20:53 +0900

    main.hにxinput.libを追加

--- main.h
+++ main.h
@@ -38,6 +38,7 @@
 #pragma comment (lib, "dxerr.lib")
 #pragma comment (lib, "dxguid.lib")
 #pragma comment (lib, "dinput8.lib")
+#pragma comment (lib, "xinput.lib")
 
 
 //*****************************************************************************
